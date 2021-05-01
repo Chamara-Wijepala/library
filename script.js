2ef826b3321ad228291b2aa330dfd1eac2cdc356
Chamara-Wijepala <mr.worf777@gmail.com>
2021-05-01 14:45:13 +0530

    Add functions to display each book on the page, these are not yet complete.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const library = document.getElementById('library');
 
 function Book(title, author, pages, read) {
     this.title = title,
@@ -8,14 +9,38 @@ function Book(title, author, pages, read) {
 };
 
 function addBookToLibrary(object) {
-    myLibrary.push(object)
+    myLibrary.push(object);
 };
 
 function displayLibrary() {
-    let library = document.querySelector('#library')
-
-    for (let book of myLibrary) {
+    //for(element in myLibrary) {
+    for (i = 0; i < myLibrary.length; i++) {
+        let book = document.createElement('div');
+        book.className = 'bookCard';
         library.appendChild(book);
+        addBookProperties(book);
     };
 };
 
+function addBookProperties(book) {// try to simplify this using object destructuring
+    let title = book.appendChild(document.createElement('h3'));
+    title.className = 'title';
+    title.innerText = myLibrary[i.title]; //access object's title property
+    let author = book.appendChild(document.createElement('h3'));
+    author.className = 'author';    
+    let pages = book.appendChild(document.createElement('h3'));
+    pages.className = 'pages';
+    let read = book.appendChild(document.createElement('button'));
+    read.className = 'read';
+};
+
+
+
+//temporary objects
+let book1 = new Book('book1', 'chamara', 100, true)
+let book2 = new Book('book2', 'chamara', 150, true)
+let book3 = new Book('book3', 'chamara', 200, false)
+addBookToLibrary(book1)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
+displayLibrary()
